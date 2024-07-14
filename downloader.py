55941d6e214291cc9a5576bd1dae313898c65eee
Ridwan Jauhar Kafabihi <145266609+RidwanJk@users.noreply.github.com>
2024-07-14 13:18:12 +0700

    fix pytube

--- downloader.py
+++ downloader.py
@@ -1,9 +1,10 @@
 from pytube import YouTube
 import moviepy.editor as mp
 import os
-    
+import time
+
 def loading(stream, file_handler, bytes_remaining):
-    total=stream.filesize
+    total = stream.filesize
     bytes_remaining = total - file_handler
     return bytes_remaining
 
@@ -26,6 +27,7 @@ def get_available_resolutions(video_url):
     except Exception as e:
         print(f"An error occurred: {e}")
         return []
+
 def get_thumbnail_url(video_url):
     try:
         yt = YouTube(video_url)
@@ -33,19 +35,22 @@ def get_thumbnail_url(video_url):
     except Exception as e:
         print(f"An error occurred: {e}")
         return ""
-    
+
 def download_video(video_url, stream):
-    try:
-        yt = YouTube(video_url, on_progress_callback=loading, on_complete_callback=finished)
-        video_file = stream.download(output_path='downloads/videos', filename=yt.title + "temp.mp4")
-        audio_file = yt.streams.get_audio_only().download(output_path='downloads', filename=yt.title + ".mp3")
+    retries = 3
+    for _ in range(retries):
+        try:
+            yt = YouTube(video_url, on_progress_callback=loading, on_complete_callback=finished)
+            video_file = stream.download(output_path='downloads/videos', filename=yt.title + "temp.mp4")
+            audio_file = yt.streams.get_audio_only().download(output_path='downloads', filename=yt.title + ".mp3")
 
-        merge_audio_video(video_file, audio_file, f"downloads/videos/{yt.title}.mp4")
+            merge_audio_video(video_file, audio_file, f"downloads/videos/{yt.title}.mp4")
 
-        os.remove(video_file)
-        os.remove(audio_file)
+            os.remove(video_file)
+            os.remove(audio_file)
 
-        return True
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return False
+            return True
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            time.sleep(2)  # Wait for 2 seconds before retrying
+    return False
